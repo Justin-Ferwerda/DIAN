f0ffecb0e7c0ce6d0dfe44c0bc7c02d3d02b47b5
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-08-18 22:04:26 -0500

    implemented search on list view

--- dianapi/views/devotion_view.py
+++ dianapi/views/devotion_view.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models import Q
 from dianapi.models import Devotion
 from dianapi.serializers import DevotionSerializer
 
@@ -14,18 +15,25 @@ def devotion_view(request, devotion_id=None, is_edit=None):
         return render(request, "devotions/edit.html", {"devotion": devotion_data})
 
     if request_method == "GET":
+
+        query = request.GET.get('q')
+
         if devotion_id:
             devotion = get_object_or_404(Devotion, pk=devotion_id)
             devotion_data = DevotionSerializer(devotion).data
             return render(request, "devotions/details.html", {"devotion": devotion_data})
         else:
-            devotions = Devotion.objects.all()
+
+            if query:
+                devotions = Devotion.objects.filter(Q(verse__icontains=query))
+            else:
+                devotions = Devotion.objects.all()
             devotion_data = DevotionSerializer(devotions, many=True).data
             items_per_page = 25
             paginator = Paginator(devotion_data, items_per_page)
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
-            return render(request, "devotions/list.html", {'page_obj': page_obj})
+            return render(request, "devotions/list.html", {'page_obj': page_obj, 'total': len(devotions), 'query': query})
 
     elif request_method == "POST":
         form_data = request.POST
