0fd0798fb69bffba8dda645cbb36a88ec43bff97
Justin Ferwerda <justin.ferwerda@gmail.com>
2025-01-17 13:20:50 -0600

    add devotion to home page

--- dianapi/views/home.py
+++ dianapi/views/home.py
@@ -1,8 +1,19 @@
+import random
+from datetime import date
 from django.shortcuts import render
+from django.db.models import Q
+from dianapi.models import Devotion
 
 def home(request):
+    current_date = date.today()
+    devotions = Devotion.objects.filter(
+    Q(date__month=current_date.month) & Q(date__day=current_date.day)
+    )
+    devotion = random.choice(devotions)
 
     return render(request, "base/index.html", {
       "subject": "I thought you might like this",
-      "body": "Body"
+      "body": "Body",
+      "verse": devotion.verse,
+      "content": devotion.content
     })
